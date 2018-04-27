937c9df18b9ee318ea9a1f099504c8f947f334dd
MeronGit <merlimuuk@gmail.com>
2018-04-27 16:18:37 +0300

    t3: Tõime projekti sisse Vue.js kasutamise ning esialgse dünaamilisuse

--- t3/js/main.js
+++ t3/js/main.js
@@ -25,3 +25,53 @@ $(function() {
         }
     });
 });
+
+let vm = new Vue({
+    el: "#app",
+    data: {
+        basePointsFactors: [
+            "Ootejärjekord",
+            "Sortimine (lohistamine või klõps)",
+            "Tähtajaline lisaülesanne",
+            "Elude kaotamine",
+            "Mängu läbikukkumine, punktid, ja kordamine",
+            "Kasutaja tähelepanu juhitakse animatsioonidega"
+        ],
+        bonusPointsFactors: [
+            "Ilus kujundus",
+            "Mängu kujundus toetab teemat",
+            "Head ilmumised",
+            "Hea sortimise tagasiside",
+            "Hea lisaülesande \"episood\"",
+            "Hea läbikukkumiste tagasiside",
+            "Heliline tagasiside",
+            "Mängu õpitavus on hea",
+            "Sorditavaid objekte saab lohistada",
+            "Töötab ka mobiilil"
+        ],
+        selectedBasePointsFactors: [],
+        selectedBonusPointsFactors: [],
+        gracePoints: 0,
+        additionalPoints: 0,
+    },
+    computed: {
+        dueDatePointsPenalty() {
+            return -5;
+        },
+        numBasePointsFactors() {
+            return this.selectedBasePointsFactors.length;
+        },
+        numBasePoints() {
+            if (this.numBasePointsFactors == 6) {
+                return 10;
+            } else if (this.numBasePointsFactors >= 4) {
+                return this.gracePoints;
+            } else {
+                return 0;
+            }
+        },
+        numBonusPoints() {
+            return this.selectedBonusPointsFactors.length + this.additionalPoints;
+        }
+    }
+});
